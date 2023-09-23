e8cee5e51d13e49b4514d0c4c8b2fdfd456f21b3
Humairaa1 <humaira71bd@gmail.com>
2023-09-23 11:41:51 +0600

    home and footer set with the help of flowbite website

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,25 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = () => {
     return (
         <div>
-            <section className="py-4 shadow-lg">Nav</section>
+            <section className="py-4 shadow-lg flex justify-between px-10">
+                <div className="text-xl font-semibold">Amajhon</div>
+                <ul className="flex gap-5 font-semibold">
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/products">Products</a>
+                    </li>
+                    <li>
+                        <a href="/dashboard">Dashboard</a>
+                    </li>
+                </ul>
+            </section>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/components/route/Router.jsx
+++ src/components/route/Router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../Home";
 import Layout from "../Layout";
 import Products from "../Products";
+import Dashboard from "../Dashboard";
 
 
 const myRouter = createBrowserRouter([
@@ -16,6 +17,10 @@ const myRouter = createBrowserRouter([
         {
           path:"/products",
           element: <Products></Products>
+        },
+        {
+          path:"/dashboard",
+          element: <Dashboard></Dashboard>
         },
       ]
     },
